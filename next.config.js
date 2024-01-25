9ba9cbd06934e097acc061b389e3afdfc15cb0e4
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-01-25 16:44:05 +0900

    nextconfig 코드 수정

--- next.config.js
+++ next.config.js
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      // Add additional paths if needed
-    };
-  },
-};
+const nextConfig = {  reactStrictMode: true,
+    swcMinify: true,
+    async rewrites() {
+      return [
+        {
+          source: "/:path*",
+          destination: "http//localhost:8080/:path*",
+        },
+      ];
+    },}
 
-module.exports = nextConfig;
+module.exports = {
+    nextConfig
+    
+}

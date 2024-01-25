be070894a65556358f1ef557ca6bc3f40e1c232b
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-01-25 16:23:00 +0900

    app/page.tsx 코드 리펙토링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,22 +8,6 @@ import Lodingpage from '@/app/loding/loding';
 import Header from '@/components/layouts/HeaderList';
 
 export default function Home() {
-  const [sessionName, setSessionName] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [headerBackground, setHeaderBackground] = useState("transparent");
-  useEffect(() => {
-    const storedSessionName = sessionStorage.getItem("pagenumber");
-    if (storedSessionName) {
-      setSessionName(storedSessionName);
-    }
-
-    const loadingTimeout = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(loadingTimeout);
-  }, []); 
-
   return (
     <>
       <div className={style.body}>

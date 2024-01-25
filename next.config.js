7c3169e8375630953491bb556a0a8d54cb75b535
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-01-25 16:47:26 +0900

    git build 에러 수정 코드

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {  reactStrictMode: true,
-    swcMinify: true,
-    async rewrites() {
-      return [
-        {
-          source: "/:path*",
-          destination: "http//localhost:8080/:path*",
-        },
-      ];
-    },}
+const { withPlausibleProxy } = require('next-plausible')
 
-module.exports = {
-    nextConfig
-    
-}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'http://localhost:8080/:path*',
+      },
+    ];
+  },
+  // 정적 HTML 내보내기 설정 추가
+  output: {
+    export: true
+  }
+};
+
+module.exports = withPlausibleProxy(nextConfig);
